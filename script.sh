7abbe3571411d8520de9e33b0b94d51066e9bd12
MohammadHasham <mhasham@sparkcognition.com>
2019-08-09 19:12:07 +0500

    Revert "initial commit"
    This reverts commit 8c8f1ef3bd6c208b207a53be145467d51cf3d8e0.

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get install ffmpeg
-
-echo Enter video path
-read videoPath
-echo Enter destination folder path
-read destFolderPath
-
-node index.js $videoPath $destFolderPath
